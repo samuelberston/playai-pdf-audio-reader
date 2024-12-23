3f5a66b604fefe15221aca2d06bf0cb70c3b9e31
Samuel Berston <samuelberston@gmail.com>
2024-12-22 21:59:19 -0800

    remove footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import MainContent from '@/components/layout/MainContent';
-import Footer from '@/components/layout/Footer';
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-      <main className="flex-1">
-        <MainContent />
-      </main>
-      <Footer />
-    </div>
+    <main className="flex-1">
+      <MainContent />
+    </main>
   );
-};
+}
